5e8f489453bf9263973ae4037be66267fc0cda21
Luan Vendt <luanvendt@gmail.com>
2024-03-06 10:26:27 -0300

    testando yfinance - Acoes Apple

---
+++ yfinance/yfinance_test.py
@@ -0,0 +1,10 @@
+import yfinance
+
+hist = yfinance.Ticker('AAPL').history(
+    period= '1d',
+    interval= '1h',
+    start= '2024-01-01',
+    end= '2024-01-03',
+    prepost= True
+)
+print(hist.head())
